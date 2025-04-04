35c35c58aef61b6e16553c730cd0700b0c2ea608
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-04-04 19:00:27 -0300

    :sparkles: feat. Screen Reset password

--- VidBuy/screens/ResetPasswordScreen.tsx
+++ VidBuy/screens/ResetPasswordScreen.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import ArrowLeft from '../assets/icons/ArrowLeft'
+import ResetForm from '../components/ResetForm/ResetForm'
+import SuccesReset from '../components/ResetForm/SuccessReset/SuccesReset'
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../navigation';
 
-export default function ResetPasswordScreen() {
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'ProfileSelect'>;
+};
+
+const ForgotPasswordScreen = ({ navigation }: Props) => {
+  const [email, setEmail] = useState<string>("");
+  const [isSent, setIsSent] = useState<boolean>(false);
+
+  const handleReset = () => {
+    setIsSent(true);
+  };
+
   return (
-    <View>
-      <Text>ResetPasswordScreen</Text>
+    <View style={styles.container}>
+       <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate("ProfileSelect")}>
+      <ArrowLeft></ArrowLeft>
+      </TouchableOpacity>
+      {isSent ? <SuccesReset /> : <ResetForm email={email} setEmail={setEmail} onReset={handleReset} />}
     </View>
-  )
-}
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  backButton: { 
+    position: "absolute",
+     top: 40, 
+     left: 20,
+     zIndex: 10
+    },
+});
+
+export default ForgotPasswordScreen;
