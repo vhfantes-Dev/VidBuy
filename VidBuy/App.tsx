e4636ee7dc91ab18127e2f129f10a1333459f54a
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-04-05 08:28:00 -0300

    :zap: att. Consumindo OneSignal

--- VidBuy/App.tsx
+++ VidBuy/App.tsx
@@ -1,20 +1,21 @@
+import React, { useEffect } from 'react';
+import { OneSignal, LogLevel } from 'react-native-onesignal';
 
-import AppNavigator from './navigation/index';
-import { LogLevel, OneSignal } from 'react-native-onesignal';
-import Constants from "expo-constants";
-import { useEffect } from 'react';
+import AppNavigator from './navigation';
 
 export default function App() {
   useEffect(() => {
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
-    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
-    OneSignal.initialize("b4ea2016-3636-4a96-a550-e20e60be8147");
-    
+    OneSignal.initialize('b4ea2016-3636-4a96-a550-e20e60be8147');
+
     OneSignal.Notifications.requestPermission(true);
     
+
+    OneSignal.Notifications.addEventListener('click', (event) => {
+      console.log('Notificação clicada:', event);
+    });
   }, []);
-  return (
-    <AppNavigator></AppNavigator>
-  );
+
+  return <AppNavigator />;
 }
